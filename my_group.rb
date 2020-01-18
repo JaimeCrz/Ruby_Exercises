62bbef7ebdf539473bdadbf65daeb19b38774541
Jaime <jaime.cruz.hostalot@gmail.com>
2020-01-18 10:20:14 +0100

    Adding Array and Hashes

---
+++ my_group.rb
@@ -0,0 +1,8 @@
+# Create an array and store variable my_group
+
+My_group = ['James', 'Sara','Tom']
+
+# Three Hashes Describing and individual name.
+person_1 = {name: 'James', gender: Male, age: 28}
+person_2 = {name: 'Sara', gender: Female, age: 22}
+person_3 = {name: 'Tom', gender: Male, age: 34}
